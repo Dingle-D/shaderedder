5eca03bd0136122c694340ae8690d2c0b0ea0cac
dogeroll <ste22irk@yandex.ru>
2025-09-27 21:54:51 +0300

    feature: чтение конфигурации из файла окружения

--- services/backend/app/main.py
+++ services/backend/app/main.py
@@ -1,25 +1,28 @@
 from fastapi import FastAPI 
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.trustedhost import TrustedHostMiddleware
 from starlette.middleware.sessions import SessionMiddleware
 from app.api.api_router import api_router
 from app.db.postgresql import init_db, close_db
 from app.db.services.users import init_users
 from app.db.services.shaders import init_shaders
+from app.security.security_headers_middleware import FrameOptionsMiddleware, CSPMiddleware
 from app.core.config import settings
+import warnings
 import asyncio
 
-app = FastAPI()
+app = FastAPI(redirect_slashes=False)
 
 app.add_middleware(
     CORSMiddleware,
-    #allow_origins=['http://localhost:8080'],
-    allow_origins=['*'],
+    allow_origins=settings.all_cors_origins,
     allow_credentials=True,
     allow_methods=['*'],
     allow_headers=['*'],
-    expose_headers=['X-Uniforms', 'Uniforms', 'X-Custom-Uniforms'],
 )
 
+app.add_middleware(FrameOptionsMiddleware)
+app.add_middleware(CSPMiddleware)
 app.add_middleware(SessionMiddleware, secret_key=settings.SECRET_KEY)
 
 @app.on_event("startup")
